abb299cc8ec42a9007a6f74f5e4261bc1711e4b8
Ivan Nadeevets <Ivan27n@gmail.com>
2022-12-02 22:48:17 +1000

    delete comments

--- hw/ViewController.swift
+++ hw/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  hw
-//
-//  Created by Надеевец on 02.12.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
